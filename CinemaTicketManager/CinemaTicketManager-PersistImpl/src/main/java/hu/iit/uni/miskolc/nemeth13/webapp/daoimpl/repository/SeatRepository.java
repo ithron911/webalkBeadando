d597128dad364332016370ed989172f9b0e07527
ithron911 <ithron911@gmail.com>
2017-05-08 21:26:15 +0200

    - Bugfix: CinemaTicketManager parent pom debugged, the controller was 2 times added as module. - Repository annotations added to the repository classes.

--- CinemaTicketManager/CinemaTicketManager-PersistImpl/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/daoimpl/repository/SeatRepository.java
+++ CinemaTicketManager/CinemaTicketManager-PersistImpl/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/daoimpl/repository/SeatRepository.java
@@ -1,9 +1,11 @@
 package hu.iit.uni.miskolc.nemeth13.webapp.daoimpl.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import hu.iit.uni.miskolc.nemeth13.webapp.daoimpl.entity.SeatEntity;
 
+@Repository
 public interface SeatRepository extends CrudRepository<SeatEntity, Integer> {
 
 }
--- CinemaTicketManager/CinemaTicketManager-PersistImpl/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/daoimpl/repository/ShowRepository.java
+++ CinemaTicketManager/CinemaTicketManager-PersistImpl/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/daoimpl/repository/ShowRepository.java
@@ -3,7 +3,9 @@ package hu.iit.uni.miskolc.nemeth13.webapp.daoimpl.repository;
 import hu.iit.uni.miskolc.nemeth13.webapp.daoimpl.entity.ShowEntity;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ShowRepository extends CrudRepository<ShowEntity, Integer>{
 
 }
