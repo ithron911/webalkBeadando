c39cce61dfdcda3b9bab04454985ee8db2035ab4
ithron911 <ithron911@gmail.com>
2017-05-08 15:04:49 +0200

    - UserDTO modified, as username and password fields removed. - UserDaoImpl and UserEntityConverter created.

--- CinemaTicketManager/CinemaTicketManager-Persist/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/dao/dto/UserDTO.java
+++ CinemaTicketManager/CinemaTicketManager-Persist/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/dao/dto/UserDTO.java
@@ -3,8 +3,6 @@ package hu.iit.uni.miskolc.nemeth13.webapp.dao.dto;
 public class UserDTO {
 
 	private int id;
-	private String username;
-	private String password;
 	private String firstname;
 	private String lastname;
 	private int age;
@@ -19,22 +17,6 @@ public class UserDTO {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getFirstname() {
