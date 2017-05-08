f05cd947b8af2883f6e3c719f522cf8cae4512f1
ithron911 <ithron911@gmail.com>
2017-05-08 21:41:03 +0200

    - EntityScan added to the persistImpl application.

--- CinemaTicketManager/CinemaTicketManager-PersistImpl/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/daoimpl/configuration/DaoImplApplication.java
+++ CinemaTicketManager/CinemaTicketManager-PersistImpl/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/daoimpl/configuration/DaoImplApplication.java
@@ -1,12 +1,14 @@
 package hu.iit.uni.miskolc.nemeth13.webapp.daoimpl.configuration;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "hu.iit.uni.miskolc.nemeth13.webapp.daoimpl")
+@EntityScan(basePackages = "hu.iit.uni.miskolc.nemeth13.webapp.daoimpl.entity")
 public class DaoImplApplication {
 
 	@Bean
