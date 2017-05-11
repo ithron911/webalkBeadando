624af7403dfe96053a837e247c4edf3aa11815c0
ithron911 <ithron911@gmail.com>
2017-05-11 20:01:02 +0200

    - TicketController fully implemented.

--- CinemaTicketShop/CinemaTicketShop-Controller/src/main/java/hu/iit/uni/miskolc/nemeth/webdev/controller/TicketController.java
+++ CinemaTicketShop/CinemaTicketShop-Controller/src/main/java/hu/iit/uni/miskolc/nemeth/webdev/controller/TicketController.java
@@ -1,9 +1,19 @@
 package hu.iit.uni.miskolc.nemeth.webdev.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import hu.iit.uni.miskolc.nemeth.webdev.controller.model.TicketRequest;
+import hu.iit.uni.miskolc.nemeth.webdev.model.Ticket;
 import hu.iit.uni.miskolc.nemeth.webdev.service.TicketService;
+import hu.iit.uni.miskolc.nemeth.webdev.service.exception.InvalidTicketResourcesException;
+import hu.iit.uni.miskolc.nemeth.webdev.service.exception.SeatBookedException;
 
 @RestController
 public class TicketController {
@@ -14,5 +24,13 @@ public class TicketController {
 	public TicketController() {
 	}
 
-	
+	@RequestMapping(value = "/buyTicket", method = RequestMethod.POST)
+	public void buyTicket(@RequestBody TicketRequest ticketRequest) throws InvalidTicketResourcesException, SeatBookedException {
+		this.ticketService.buyTicket(ticketRequest.getUserId(), ticketRequest.getShowId(), ticketRequest.getSeatId());
+	}
+
+	@RequestMapping(value = "/buyTicket/{userId}", method = RequestMethod.POST)
+	List<Ticket> getTicketsByUserId(@PathVariable("userId") int userId) {
+		return this.ticketService.getTicketsByUserId(userId);
+	}
 }
