a544f8e3de0177d4b46b90d675d12547ee50456a
ithron911 <ithron911@gmail.com>
2017-05-12 01:45:54 +0200

    - CinemaTicketMAnager removed.

--- CinemaTicketManager/CinemaTicketManager-Persist/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/dao/ShowDao.java
+++
@@ -1,9 +0,0 @@
-package hu.iit.uni.miskolc.nemeth13.webapp.dao;
-
-import java.util.List;
-
-import hu.iit.uni.miskolc.nemeth13.webapp.dao.dto.ShowDTO;
-
-public interface ShowDao {
-	List<ShowDTO> listShowsByMovieAndCinemaId(int movieId, int cinemaId);
-}
--- CinemaTicketManager/CinemaTicketManager-Persist/src/main/java/hu/iit/uni/miskolc/nemeth13/webapp/dao/TicketDao.java
+++
@@ -1,7 +0,0 @@
-package hu.iit.uni.miskolc.nemeth13.webapp.dao;
-
-import hu.iit.uni.miskolc.nemeth13.webapp.dao.dto.TicketDTO;
-
-public interface TicketDao {
-	void buyTicket(TicketDTO ticketDTO);
-}
