46c285f7c48b1f9ffb92cda82c421b2bbecf3426
ithron911 <ithron911@gmail.com>
2017-05-11 14:51:21 +0200

    - ServiceAppConfig created, and service modul added to the implementation as dependency.

---
+++ CinemaTicketShop/CinemaTicketShop-ServiceImpl/src/main/java/hu/iit/uni/miskolc/nemeth/webdev/serviceimpl/configuration/ServiceApplicationContextConfig.java
@@ -0,0 +1,9 @@
+package hu.iit.uni.miskolc.nemeth.webdev.serviceimpl.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("hu.iit.uni.miskolc.nemeth.webdev.serviceimpl")
+public class ServiceApplicationContextConfig {
+}
